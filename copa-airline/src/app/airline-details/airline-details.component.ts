bc8a1f75521fdae92db57dc9f6d5a94b6e3f9e16
Harsha Anantharamu <45288017+Harsha-A@users.noreply.github.com>
2021-05-22 18:19:50 +0530

    styles : bgcolor, formstyle and button added

--- copa-airline/src/app/airline-details/airline-details.component.ts
+++ copa-airline/src/app/airline-details/airline-details.component.ts
@@ -8,21 +8,58 @@ import { FormsModule } from '@angular/forms';
 })
 export class AirlineDetailsComponent implements OnInit {
 
-  userForm!: FormGroup;
+  flightForm!: FormGroup;
+  segmentForm!: FormGroup;
   constructor(public formBuilder: FormBuilder, public flightform: FormsModule) { }
-flightnumber : string = "I am sample text";
-active = "'nav-menu1'";
+  active = 1;
+  flight =  {
+      groupId:0,
+      version:'',
+      flightno:''
+    };  
+  
   ngOnInit(): void {
+    this.flight.groupId = 20210522;
+    this.flight.version = 'V0001';
+    this.flight.flightno = 'CM 101'
     
+    this.flightForm = this.formBuilder.group({
+      recordstatus: ['', [Validators.required, Validators.minLength(4)]],
+      airlinecc: ['', [Validators.required]],
+      flightnumber: ['', [Validators.required]],
+      frequencyrate: ['', [Validators.required]],
+      scheffdate: ['', [Validators.required, Validators.minLength(4)]],
+      schdisdate: ['', [Validators.required]],
+      schfrequency: ['', [Validators.required]],
+      timemode: ['', [Validators.required]]
+    }) 
+
+    this.segmentForm = this.formBuilder.group({
+      arrivalcity: ['', [Validators.required, Validators.minLength(4)]],
+      aircraftsta: ['', [Validators.required]],
+      paxsta: ['', [Validators.required]],
+      depterminal: ['', [Validators.required]],
+      arrterminal: ['', [Validators.required, Validators.minLength(4)]],
+      aircraftconfig: ['', [Validators.required]],
+      aircrafttype: ['', [Validators.required]],
+      departurecity: ['', [Validators.required]],
+      aircraftstd: ['', [Validators.required]],
+      paxstd: ['', [Validators.required, Validators.minLength(4)]],
+      deptimevarfromutc: ['', [Validators.required]],
+      arrtimevarfromutc: ['', [Validators.required]],
+      inflightadj: ['', [Validators.required]],
+      eqpsertype: ['', [Validators.required]]
+    }) 
   }
 
    
    getControl(){
-    return this.userForm.controls;
+    return this.flightForm.controls;
   }
   
   onSubmit(){
-    console.log(this.userForm);
+    console.log(this.flightForm);
+    console.log(this.segmentForm)
   }
   
 }
